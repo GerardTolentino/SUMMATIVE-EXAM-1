713bb1efa889a919b4e85eeb6653a539f78afb48
Gerard Tolentino <69785555+GerardTolentino@users.noreply.github.com>
2021-01-08 08:40:39 +0800

    Create script1.js

---
+++ script1.js
@@ -0,0 +1,17 @@
+function myself(firstname,lastname,age,subject)
+{   this.firstname=firstname;
+    this.lastname=lastname;
+    this.age=age;
+    this.subject=subject;
+    this.introduce= function introduce(){
+        console.log(`Hello, I am ${this.firstname} ${this.lastname} `)
+    }
+    this.enrolled=function enrolled(){
+        console.log(`I am enrolled in ${this.subject}`)
+    }
+
+}
+
+let myself1= new myself("Gerard","Tolentino",20,"ISC2609");
+myself1.introduce();
+myself1.enrolled();
