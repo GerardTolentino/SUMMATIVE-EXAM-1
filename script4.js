6197a901c381e7f6bf4c721c8fdbdf9e0abc93c3
Gerard Tolentino <69785555+GerardTolentino@users.noreply.github.com>
2021-01-08 08:47:37 +0800

    Create script4.js

---
+++ script4.js
@@ -0,0 +1,13 @@
+class Line{
+    constructor(x1,y1,x2,y2){
+         this.x1=x1;
+         this.y1=y1;
+         this.x2=x2;
+         this.y2=y2;
+         
+        
+         console.log(this.x1+this.y1+this.x2+this.y2);  
+     }
+ }
+ var comp= new Line(1,2,3,4);
+ 
