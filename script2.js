d49da61c65b47fa6a846347bc0d8fab2836da89d
Gerard Tolentino <69785555+GerardTolentino@users.noreply.github.com>
2021-01-08 08:44:59 +0800

    Create script2.js

---
+++ script2.js
@@ -0,0 +1,9 @@
+function greet(name,message){
+    if (name=="Juan Dela Cruz" && message=="Good morning"){
+        console.log(`${message} Juan!`);
+    }else {
+    return false
+    }
+}
+let input = new greet("Juan Dela Cruz","Good morning");
+input.greet();
